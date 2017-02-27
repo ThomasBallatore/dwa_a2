5026f639d134d1f15a32dd658d3313f255dceaa6
Thomas Ballatore <ballatore@lakebasin.org>
2017-02-27 21:21:55 +0900

    test index page

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>My Encryption Super System</title>
+	<meta charset="utf-8">
+
+	<!-- <meta name="viewport" content="width=device-width"> -->
+
+	<!-- <link rel="stylesheet" href="css/a1.css"> -->
+
+</head>
+<body>
+
+	hey there
+
+</body>
+</html>
